7b3c1f2ff4ae57a87bc7535b46ee537fc438318f
nithish1018 <nithishsiliveru18@gmail.com>
2023-11-10 15:39:12 +0530

    added tap on game images

--- app.js
+++ app.js
@@ -7,25 +7,11 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "/public")));
 const gameList = require("./utils");
-app.get('/home',(request,response)=>{
-    response.render('index')
-})
-app.get('/next',(request,response)=>{
-
-    response.render('next');
-})
 app.get("/:name", (req, res) =>{
     const gameName = req.params.name;
-    console.log(gameName + "ewhbhdbw")
-    console.log(gameList[0].gamename + "jgvjvjv")
-    const thisGame= gameList.filter(game =>game.gamename===gameName);
-    const gameCreator = req.params.creator;
-    console.log(thisGame.gamename + "whkebkbw");
     
     res.render("onegame", {
         title: gameName,
-        author: gameCreator,
-        thisGame:thisGame,
         gamelist: gameList,
         gameName:gameName
 
